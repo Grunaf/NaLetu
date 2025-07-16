f7694a6539764ac088cd7125bee87f28f00f9614
Grunaf <roperar@tutanota.de>
2025-07-16 14:59:42 +0300

    fix: add missing Python requirements, switch image to gunicorn, add @source for HTML in tailwind, enable vite prod mode

--- flaskr/routes/assets_blueprint.py
+++ flaskr/routes/assets_blueprint.py
@@ -1,15 +1,35 @@
+import json
 from flask import Blueprint
 from config import DevConfig, IS_DEV
 
-mod = Blueprint("assets ", __name__, static_url_path="/asses")
+mod = Blueprint(
+    "assets ",
+    __name__,
+    static_folder="../static/assets_compiled/bundled",
+    static_url_path="/assets/bundled",
+)
+
+manifest = {}
+if not IS_DEV:
+    manifest_path = "flaskr/static/assets_compiled/manifest.json"
+    try:
+        with open(manifest_path, "r") as content:
+            manifest = json.load(content)
+    except OSError as exception:
+        raise OSError(
+            f"Manifest file not found at {manifest_path}. Run `npm run build`."
+        ) from exception
 
 
 @mod.app_context_processor
 def add_context():
-    def dev_assets(filepath):
-        return f"{DevConfig.VITE_DEV_SERVER}/flaskr/static/{filepath}"
+    def dev_assets(file_path):
+        return f"{DevConfig.VITE_DEV_SERVER}/assets/{file_path}"
 
-    def prod_assets():
-        pass
+    def prod_assets(file_path):
+        try:
+            return f"/assets/{manifest[file_path]['file']}"
+        except:
+            return "asset-not-found"
 
     return {"asset": dev_assets if IS_DEV else prod_assets, "is_dev": IS_DEV}
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,25 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
-import { resolve } from "path";
+import path, { resolve } from "path";
 
 export default defineConfig({
+  root: "./flaskr/static/",
+  base: "/assets/",
   build: {
     manifest: "manifest.json",
-    outDir: resolve("./flaskr/static/assets"),
+    outDir: resolve("./flaskr/static/assets_compiled/"),
+    assetsDir: "bundled",
     rollupOptions: {
-      input: {
-        test: resolve("./flaskr/static/js/main.js"),
-      },
+      input: [
+        resolve("./flaskr/static/js/main.js"),
+        resolve("./flaskr/static/js/catalog/catalog.js"),
+        resolve("./flaskr/static/js/trip-setup.js"),
+        resolve("./flaskr/static/js/itinerary/trip-itinerary.js"),
+        resolve("./flaskr/static/styles/styles.css"),
+      ],
     },
+    emptyOutDir: true,
+    copyPublicDir: false,
   },
   plugins: [tailwindcss()],
 });
